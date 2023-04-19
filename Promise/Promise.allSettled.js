f1191d8d0d07497e37d6f168bf4ce3edae80494f
alamin <alamin.shaheen@ideascale.com>
2023-04-19 12:36:14 +0600

    Implemented Promise.allSettled.js

---
+++ Promise/Promise.allSettled.js
@@ -0,0 +1,28 @@
+import {createPromise} from "../Utilities.js";
+
+// ● Map the array of promises to return an object with status and value/error depending upon the promised settlement.
+// ● Pass this map to the Promise.all to run them at once and return the result.
+
+/**
+ *
+ * @param promises - Array of promises or any other non promise values
+ * @returns {Promise<Array<any>>}
+ */
+const promiseAllSettled = (promises) => {
+    const settledPromises = promises.map(promise => {
+        return Promise
+            .resolve(promise)
+            .then(value => ({status: "fulfilled", value}))
+            .catch(error => ({status: "rejected", value: error}));
+    })
+    return Promise.all(settledPromises);
+}
+
+promiseAllSettled([
+    createPromise(1000),
+    createPromise(2000),
+    createPromise(5020),
+    56,
+    Promise.resolve(45),
+    Promise.reject(29),
+]).then(console.log).catch(console.error);
